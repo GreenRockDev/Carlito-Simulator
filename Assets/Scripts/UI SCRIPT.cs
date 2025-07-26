e5d4e424772549a9aa25c2b81d9e9df4a6aa6793
mrmetapod2 <hectormzacrias@gmail.com>
2025-07-26 18:59:56 -0300

    esto es inutil ignorar

--- Assets/Scripts/UI SCRIPT.cs
+++ Assets/Scripts/UI SCRIPT.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 public class UISCRIPT : MonoBehaviour
 {
@@ -8,12 +9,25 @@ public class UISCRIPT : MonoBehaviour
     public Transform menu2;
     public Transform menu3;
 
-    public float transitionTime = 0.3f;
-    public Vector3 visiblePos = new Vector3(0, 0, 0);       // se dicta a donde van cuando estan en uso y cuando no
-    public Vector3 hiddenPos = new Vector3(0, 10f, 0);      // y se dicta cuanto tiempo van a tardar
+    public List<Transform> menu1Buttons; // se asignan los botones
+    public List<Transform> menu2Buttons;
+    public List<Transform> menu3Buttons;
+
+    public List<Transform> menu1Sprites;
+    public List<Transform> menu2Sprites;// y estos son los sprites
+    public List<Transform> menu3Sprites;
+
+
+    public float transitionTime = 0.3f;                     // se dicta cuanto tiempo van a tardar
+    public Vector3 visiblePos = new Vector3(0, 0, 0);       //y se dicta a donde van cuando estan en uso y cuando no
+    public Vector3 hiddenPos = new Vector3(0, 10f, 0);      
+    public Vector3 buttonOffset = new Vector3(0, -5, 0);
+    public Vector3 spriteOffset = new Vector3(0, -5, 0);// esto es cuanto se tiene que mover un sprite
 
     private int currentMenu = 0; // menu inicial osea el mostrador
-    private Transform[] menus;
+    private Transform[] menus; //lista de los menus
+    private List<Transform>[] menuButtons;  //lista de todos los objetos con los menus
+    private List<Transform>[] menuSprites;
     private Coroutine currentTransition;
 
     void Start()
@@ -21,11 +35,22 @@ public class UISCRIPT : MonoBehaviour
     {
         //se crea una lista con todos los menus
         menus = new Transform[] { null, menu1, menu2, menu3 };
+        menuButtons = new List<Transform>[] { null, menu1Buttons, menu2Buttons, menu3Buttons };
+        menuSprites = new List<Transform>[] { null, menu1Sprites, menu2Sprites, menu3Sprites };
 
         // se esconden todos
         for (int i = 1; i < menus.Length; i++)
         {
             menus[i].position = hiddenPos;
+
+            foreach (var btn in menuButtons[i])
+            {
+                btn.position += hiddenPos; 
+            }
+            foreach (var sprite in menuSprites[i])
+            {
+                sprite.position += hiddenPos;
+            }
         }
     }
 
@@ -42,11 +67,23 @@ public class UISCRIPT : MonoBehaviour
 
     private IEnumerator SwitchMenus(int newIndex) // esta funcion se encarga de cambiar el menu
     {
+        List<Coroutine> running = new List<Coroutine>();
         // el actual menu se mueve arriva si no es el mostrador
         if (currentMenu != 0)
         {
             Transform current = menus[currentMenu];
             yield return StartCoroutine(SmoothMove(current, hiddenPos)); //llendo a hiddenpos
+
+            foreach (var btn in menuButtons[currentMenu])
+            {
+                Vector3 upPos = btn.position - buttonOffset;
+                running.Add(StartCoroutine(SmoothMove(btn, upPos)));
+            }
+            foreach (var Sprite in menuSprites[currentMenu])
+            {
+                Vector3 upPos = Sprite.position - spriteOffset;
+                running.Add(StartCoroutine(SmoothMove(Sprite, upPos)));
+            }
         }
 
         // y se mueve el nuevo menu abajo si no es el mostrador
@@ -54,8 +91,18 @@ public class UISCRIPT : MonoBehaviour
         {
             Transform next = menus[newIndex];
             yield return StartCoroutine(SmoothMove(next, visiblePos));
+            foreach (var btn in menuButtons[newIndex])
+            {
+                Vector3 downPos = btn.position + buttonOffset;
+                running.Add(StartCoroutine(SmoothMove(btn, downPos)));
+            }
+            foreach (var Sprite in menuSprites[newIndex])
+            {
+                Vector3 downPos = Sprite.position + spriteOffset;
+                running.Add(StartCoroutine(SmoothMove(Sprite, downPos)));
+            }
         }
-
+        foreach (var c in running) yield return c;
         currentMenu = newIndex; // se cambia cual es el menu actual
     }
 
